0a71f77883ab52a9406f1767adf010ff54087702
joaogmelo95 <joaogmelo95@gmail.com>
2021-08-25 16:04:40 -0300

    Introdução à JavaScript

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-javascript/exercicio-1/script.js
@@ -0,0 +1,15 @@
+const myName = 'João';
+const birthCity = 'Recife';
+let birthYear = 1995;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = 'Brasília';
+
+console.log(birthCity);
