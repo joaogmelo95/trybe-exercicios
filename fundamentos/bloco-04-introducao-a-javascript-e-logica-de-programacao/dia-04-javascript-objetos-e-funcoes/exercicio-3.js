209fbf5f2658517ccd891cb25efcde6a0584bd7d
joaogmelo95 <joaogmelo95@gmail.com>
2021-08-30 14:52:02 -0300

    Exercícios de objetos e funções antes da aula ao vivo

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicio-3.js
@@ -0,0 +1,25 @@
+function somar(a, b) {
+    console.log(a + b);
+}
+
+function subtrair(a, b) {
+    console.log(a - b);
+}
+
+function multiplicar(a, b) {
+    console.log(a * b);
+}
+
+function dividir(a, b) {
+    console.log(a / b);
+}
+
+function modular(a, b) {
+    console.log(a % b);
+}
+
+somar(3, 2);
+subtrair(3,2);
+multiplicar(3,2);
+dividir(6,3);
+modular(2,2);
