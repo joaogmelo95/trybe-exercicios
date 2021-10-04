d5881835296a5da4c23ed3351d6f9cc18096156a
joaogmelo95 <joaogmelo95@gmail.com>
2021-10-04 15:59:51 -0300

    Bloco 8 Dia 1 - exercício antes da aula ao vivo

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/exercicio-1/script.js
@@ -0,0 +1,9 @@
+const wakingUp = () => 'Acordando!!';
+
+const letsCoffee = () => 'Bora tomar café!!';
+
+const letsSleep = () => 'Partiu dormir!!';
+
+const doingThings = (func) => console.log(func());
+
+doingThings(letsCoffee);
