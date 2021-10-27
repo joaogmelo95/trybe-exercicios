1586cffa738adca867b4268dc60fd1a436e7507d
joaogmelo95 <joaogmelo95@gmail.com>
2021-10-27 16:16:08 -0300

    Bloco 10 Dia 1 - exercício antes da aula ao vivo

---
+++ front-end/dia-1-hello-world-no-react/testando-meu-computador/src/MyName.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class MyName extends Component {
+  render() {
+    return <div>
+      <h1>João Melo</h1>
+      <p>Estudante</p>
+    </div>
+  }
+}
+
+export default MyName;
